8d19a258ca58c8fdb4990f8f3dd99690dbcdcea3
Jenni Tran <jenni.tran@student.laurea.fi>
2023-03-12 00:06:21 +0200

    added guestbook

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Import Express library
 const express = require("express");
 
+const fs = require("fs");
+
 // Create express app
 const app = express();
 const port = 3000;
@@ -11,11 +13,22 @@ app.engine("html", require("ejs").renderFile);
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("pages/frontpage.html");
+  res.render("pages/frontpage");
 });
 
+/**
+ * "/guestbook" –route will load a JSON-file and parse it on the page as a formatted table
+ * (use either Bootstrap or Pure.css to style the table).
+ * Sample JSON file is available here; http://pastebin.com/VpbJqSicLinks
+ */
 app.get("/guestbook", (req, res) => {
-  res.send("Having Guestbook!");
+  fs.readFile("guestBookData.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    const parsedData = JSON.parse(data);
+    res.render("pages/guestBook", { data: parsedData });
+  });
 });
 
 app.get("/newmessage", (req, res) => {
