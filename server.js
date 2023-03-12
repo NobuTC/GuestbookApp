23aca650b2c8542b217b7b1c09a63f3cb4f8ecd6
Jenni Tran <jenni.tran@student.laurea.fi>
2023-03-12 22:04:26 +0200

    added newMessage

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // Import Express library
 const express = require("express");
-
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 // Create express app
 const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 const port = 3000;
+const dataFile = "guestBookData.json";
 
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
@@ -22,7 +29,7 @@ app.get("/", (req, res) => {
  * Sample JSON file is available here; http://pastebin.com/VpbJqSicLinks
  */
 app.get("/guestbook", (req, res) => {
-  fs.readFile("guestBookData.json", "utf8", function (err, data) {
+  fs.readFile(dataFile, "utf8", function (err, data) {
     if (err) {
       console.log(err);
     }
@@ -32,7 +39,36 @@ app.get("/guestbook", (req, res) => {
 });
 
 app.get("/newmessage", (req, res) => {
-  res.send("newMessage is here!");
+  res.render("pages/newMessage");
+});
+
+//receiving data from form.
+app.post("/newmessage", (req, res) => {
+  const username = req.body.username;
+  const country = req.body.country;
+  const message = req.body.message;
+
+  fs.readFile(dataFile, "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+    }
+    const parsedData = JSON.parse(data);
+    parsedData.push({
+      username,
+      country,
+      message,
+      id: parsedData.length + 1,
+    });
+
+    fs.writeFile(dataFile, JSON.stringify(parsedData), (error) => {
+      if (error) {
+        console.error(`There is error!`);
+      } else {
+        console.log(`Data is succesfully saved!`);
+        res.redirect("/guestbook");
+      }
+    });
+  });
 });
 
 app.get("/ajaxmessage", (req, res) => {
